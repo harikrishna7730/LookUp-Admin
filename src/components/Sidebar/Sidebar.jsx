27b36a2826d4ffe03a63a3c97e75b8c04d69fe7e
harikrishna7730 <abhitangala@gmail.com>
2025-04-22 23:41:04 +0530

    added /3

--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -6,7 +6,6 @@ import list_product_icon from "../../assets/Product_list_icon.svg"
 const Sidebar = () => {
   return (
     <div className='sidebar'>
-      
       <Link to={'/addproduct'} style={{textDecoration:"none"}}>
       <div className="sidebar-item">
             <img src={add_product_icon} alt="" />
