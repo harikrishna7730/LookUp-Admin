5468939127c853d53eec7ff9b4092f7be652cfe2
harikrishna7730 <abhitangala@gmail.com>
2025-04-22 22:57:55 +0530

    added /2

--- src/components/Pages/Admin.jsx
+++ src/components/Pages/Admin.jsx
@@ -4,12 +4,14 @@ import Sidebar from '../Sidebar/Sidebar'
 import { Route, Routes } from 'react-router-dom'
 import AddProduct from '../AddProduct/AddProduct'
 import ListProduct from '../ListProduct/ListProduct'
+import Home from './Home'
 
 const Admin = () => {
   return (
     <div className='admin'>
       <Sidebar/>
       <Routes>
+        <Route path='/' element={<Home/>}/>
         <Route path='/addproduct' element={<AddProduct/>}/>
         <Route path='/listproduct' element={<ListProduct/>}/>
       </Routes>
--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -6,10 +6,7 @@ import list_product_icon from "../../assets/Product_list_icon.svg"
 const Sidebar = () => {
   return (
     <div className='sidebar'>
-       <Link to={"/"}> <div className="sidebar-item">
-            <img src={add_product_icon} alt="" />
-            <p>Home</p>
-        </div></Link>
+      
       <Link to={'/addproduct'} style={{textDecoration:"none"}}>
       <div className="sidebar-item">
             <img src={add_product_icon} alt="" />
